711e7b6d7cd9e76b90665db46e997844f596a410
Nika Kakhiani <nikakakhi@gmail.com>
2023-06-23 01:51:24 +0400

    customer profile update

--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "menu",
     "marketplace",
     "django.contrib.gis",
+    "customers",
 ]
 
 MIDDLEWARE = [
@@ -73,6 +74,7 @@ TEMPLATES = [
                 "accounts.context_processors.get_google_api",
                 "marketplace.context_processors.get_cart_counter",
                 "marketplace.context_processors.get_cart_amounts",
+                "accounts.context_processors.get_user_profile",
             ],
         },
     },
