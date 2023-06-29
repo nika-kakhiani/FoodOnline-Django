73133d3ae9d4fee5634ad16bf05555a9fc534161
Nika Kakhiani <nikakakhi@gmail.com>
2023-06-29 20:32:39 +0400

    customer dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from vendor.models import Vendor
 from django.template.defaultfilters import slugify
+from orders.models import Order
 
 # Create your views here.
 
@@ -164,7 +165,14 @@ def myAccount(request):
 @login_required(login_url="login")
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request, "accounts/custDashboard.html")
+    orders = Order.objects.filter(user=request.user, is_ordered=True)
+    recent_orders = orders[:5]
+    context = {
+        "orders": orders,
+        "orders_count": orders.count(),
+        "recent_orders": recent_orders,
+    }
+    return render(request, "accounts/custDashboard.html", context)
 
 
 @login_required(login_url="login")
